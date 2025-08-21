ca6bcfc682ce58a4e105ae4d9f6a01f81cc556de
Maxim <glin.maxim@yandex.ru>
2025-08-21 23:23:21 +0300

    init: Инициализирует приложение

---
+++ go.mod
@@ -0,0 +1,5 @@
+module first-service-go
+
+go 1.23.2
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	_ "github.com/lib/pq"
+)
+
+type Response struct {
+	Status string `json:"status"`
+    Message string `json:"message"`
+	Data string `json:"data,omitempty"`
+}
+
+var db *sql.DB
+
+
+func getInfoAbout(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    response := Response{Message: "Hello, World!", Status: "success"}
+    json.NewEncoder(w).Encode(response)
+}
+
+func getInfoFromDb(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	count := 0
+
+    row := db.QueryRow("SELECT count(*) as count from cars_csv;")
+	err := row.Scan(&count)
+
+	// Если ошибка 
+    if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		response := Response{Message: "Неизвестная ошибка сервера", Status: "error"}
+		json.NewEncoder(w).Encode(response)
+		fmt.Println(err)
+		return
+    }
+
+	w.WriteHeader(http.StatusOK)
+    response := Response{Message: strconv.Itoa(count), Status: "success"}
+    json.NewEncoder(w).Encode(response)
+	
+}
+
+func main() {
+	connStr := "user=# password=# dbname=# sslmode=disable port=5433"
+	var err error
+	db, err = sql.Open("postgres", connStr)
+
+	if err != nil {
+		fmt.Println("Нет подключения к базе данных!")
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	err = db.Ping()
+    if err != nil {
+		fmt.Println("Нет подключения к базе данных!")
+		fmt.Println(err)
+		return
+    }
+
+    http.HandleFunc("/about", getInfoAbout)
+	http.HandleFunc("/info", getInfoFromDb)
+
+	fmt.Println("Server is listening...")
+    http.ListenAndServe(":8080", nil)
+}
